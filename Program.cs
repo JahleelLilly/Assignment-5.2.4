d6d7954ca78c0497c56fabd9b0c52686a7ddf307
Jahle <Jahle@ABLACKMAN>
2024-06-05 15:57:58 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Assignment_5._2._4
+{
+    class Program
+    {
+        static bool IsPalindrome(string s)
+        {
+
+            if (s.Length <= 1)
+                return true;
+
+            if (char.ToLower(s[0]) != char.ToLower(s[s.Length - 1]))
+                return false;
+
+            return IsPalindrome(s.Substring(1, s.Length - 2));
+        }
+
+        static void Main(string[] args)
+        {
+            Console.Write("Input a string: ");
+            string input = Console.ReadLine();
+
+            bool isPalindrome = IsPalindrome(input);
+            if (isPalindrome)
+                Console.WriteLine("The string is Palindrome.");
+            else
+                Console.WriteLine("The string is not Palindrome.");
+        }
+
+    }
+}
+
